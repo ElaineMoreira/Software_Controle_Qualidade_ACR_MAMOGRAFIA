0c6f04c3461fec4a4a985288182ecdcbe6e0ecb3
Elaine <elainefelipemoreira@gmail.com>
2022-03-11 18:52:04 -0300

    Adiciona logo da Radiologia

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="http://2.bp.blogspot.com/-BBvEJNzRVu8/VLL4HKoYd5I/AAAAAAAABEY/NbJXkaK1vy0/s1600/rrad.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Software para Controle de Qualidade ACR em Mamografia
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ElaineMoreira/Software_Controle_Qualidade_ACR_MAMOGRAFIA"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link para repositório
         </a>
       </header>
     </div>
